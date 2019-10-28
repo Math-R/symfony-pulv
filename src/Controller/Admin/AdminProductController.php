0ce4cf06a32f3928487c8bd60cde2506639eb2fd
Mathieu Richard <mathieu@comeet.in>
2019-10-28 16:04:31 +0100

    edition start

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -8,8 +8,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminProductController extends AbstractController
 {
@@ -23,9 +25,23 @@ class AdminProductController extends AbstractController
         $this->repository = $repository;
     }
 
+    /**
+     * @Route("/admin", name="admin.product.index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function index()
     {
         $products = $this->repository->findAll();
         return $this->render('admin/product/index.html.twig', compact('products'));
+    }
+
+    /**
+     * @Route("/admin/{id}", name="admin.product.edit")
+     * @param Product $product
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Product $product)
+    {
+        return $this->render('admin/product/edit.html.twig', compact('products'));
     }
 }
