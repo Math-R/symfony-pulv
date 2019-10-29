1e6354552b9a2de347021901519ed86f35257192
Mathieu Richard <mathieu@comeet.in>
2019-10-29 10:31:29 +0100

    add edit and create form route functions

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -9,20 +9,28 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @property  em
+ */
 class AdminProductController extends AbstractController
 {
     /*
      * @var ProductRepository
      */
     private $repository;
+    private $em;
 
-    public function __construct(ProductRepository $repository)
+    public function __construct(ProductRepository $repository, ObjectManager $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
     }
 
     /**
@@ -35,13 +43,47 @@ class AdminProductController extends AbstractController
         return $this->render('admin/product/index.html.twig', compact('products'));
     }
 
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/admin/product/create", name="admin.product.create")
+     * @throws \Exception
+     */
+    public function store(Request $request){
+
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($product);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.product.index');
+        }
+        return $this->render('admin/product/create.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
     /**
      * @Route("/admin/{id}", name="admin.product.edit")
      * @param Product $product
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function edit(Product $product)
+    public function edit(Product $product, Request $request)
     {
-        return $this->render('admin/product/edit.html.twig', compact('products'));
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            return $this->redirectToRoute('admin.product.index');
+        }
+        return $this->render('admin/product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
     }
 }
